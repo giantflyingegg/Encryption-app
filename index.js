c2dd491d6723d77b7b9aad532b2ac8d09bd7aca4
giantflyingegg <abwaham@gmail.com>
2023-10-05 20:41:36 +0100

    Initial commit, started logic

---
+++ index.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('input').value;
+const output = document.getElementById('output').value;
+// const key = document.getElementById('key').textContent.split('');
+
+console.log(input);
+
+function encrypt(input, key) {
+  let result = "";
+  const textArray = input.replace(/\s/g, '').split('');
+    for (let i=0; i<textArrray.length; i++) {
+      textArray[i].charCodeAt(0) + 1;
+    }
+}
