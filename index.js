af8b20d64c1749fff17eca49d8a429c653c4d4be
giantflyingegg <abwaham@gmail.com>
2023-10-13 10:13:11 +0100

    styling started and cut disabled as app breaking

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-const input = document.getElementById('input').value;
-const output = document.getElementById('output').value;
-// const key = document.getElementById('key').textContent.split('');
 
-console.log(input);
+//get buttons
+const encryptButton = document.getElementById('encryptButton');
+const decryptButton = document.getElementById('decryptButton');
 
-function encrypt(input, key) {
-  let result = "";
-  const textArray = input.replace(/\s/g, '').split('');
-    for (let i=0; i<textArrray.length; i++) {
-      textArray[i].charCodeAt(0) + 1;
-    }
+//encrypt function
+function encrypt() {
+
+  let inputText = document.getElementById('input').value;
+  console.log(inputText);
+  let key = document.getElementById('key').value;
+
+  //check if key is a number
+  if (!/^\d+$/.test(key)) {
+    alert("Key must only contain numeric characters!");
+    return;}
+
+  let encryptedText = '';
+  let resultArr = [];
+  let keyStr = key.toString();
+  let keyArr = keyStr.split("").map(Number);
+  let j = 0;
+
+  for (let i = 0; i < inputText.length; i++) {
+    let charCode = inputText.charCodeAt(i);
+    resultArr.push(charCode - keyArr[j]);
+     j = (j+1) % keyArr.length; 
+  } return String.fromCharCode(...resultArr);
 }
+
+//decrypt function
+function decrypt() {
+
+  let inputText = document.getElementById('input').value;
+  console.log(inputText);
+  let key = document.getElementById('key').value;
+
+   //check if key is a number
+   if (!/^\d+$/.test(key)) {
+    alert("Key must only contain numeric characters!");
+    return;}
+
+  let decryptedText = '';
+  let resultArr = [];
+  let keyStr = key.toString();
+  let keyArr = keyStr.split("").map(Number);
+  let j = 0;
+
+  for (let i = 0; i < inputText.length; i++) {
+    let charCode = inputText.charCodeAt(i);
+     resultArr.push(charCode + keyArr[j]);
+     j = (j+1) % keyArr.length; 
+     
+  } return String.fromCharCode(...resultArr);
+}
+
+//event listeners
+encryptButton.addEventListener('click', () => {
+  console.log('encrypt button clicked');
+  const outputText = encrypt();
+  document.getElementById('output').innerHTML = outputText;
+});
+
+decryptButton.addEventListener('click', () => {
+  console.log('decrypt button clicked');
+  const outputText = decrypt();
+  document.getElementById('output').innerHTML = outputText;
+});
+
+document.getElementById('output').addEventListener('cut', (e) => {
+  e.preventDefault();
+});
+
+  
